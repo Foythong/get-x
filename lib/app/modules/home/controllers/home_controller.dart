c8a20877dd52ede357635ebb9d11467120029edc
kwanmanas.d <kwanmanas.d@gethergo.com>
2024-10-16 21:42:57 +0700

    home scoll

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_tutorial/app/data/models/api_model.dart';
 import 'package:getx_tutorial/app/data/providers/api_provider.dart';
@@ -9,10 +10,27 @@ class HomeController extends GetxController {
 
   final products = <Product>[].obs;
 
+  var perPage = 20; // จำนวนผลิตภัณฑ์ที่จะโหลดต่อครั้ง
+  var countPage =
+      0; // ใช้ติดตามจำนวนหน้าที่โหลดแล้ว (เป็นตัวนับสำหรับตำแหน่งเริ่มต้นของรายการผลิตภัณฑ์ที่กำลังจะโหลด)
+  final ScrollController scrollController =
+      ScrollController(); // ควบคุมการเลื่อนของ ScrollView
+
+  RxBool isLoading = false.obs;
+
   @override
   void onInit() {
     super.onInit();
+
     getProducts();
+
+    scrollController.addListener(() {
+      if (scrollController.offset >=
+              scrollController.position.maxScrollExtent &&
+          !scrollController.position.outOfRange) {
+        getProducts();
+      }
+    });
   }
 
   @override
@@ -26,11 +44,34 @@ class HomeController extends GetxController {
   }
 
   void getProducts() async {
-    var result = await Get.find<ApiProvider>().getApi();
-    var data = result?.products ?? [];
+    try {
+      isLoading(true);
 
-    products.clear();
-    products.addAll(data);
+      var result = await Get.find<ApiProvider>().getApi();
+      var data = result?.products ?? [];
+
+      // ถ้า data.length น้อยกว่าจำนวนที่โหลดไปแล้ว ให้หยุดการทำงานทันที
+      if (data.length <= countPage) {
+        isLoading(false); // หยุดการโหลด
+        return; // หยุดการทำงานของฟังก์ชัน
+      }
+
+      // โหลดผลิตภัณฑ์เพิ่มจากข้อมูลที่ได้มาและเพิ่มเข้าไปในรายการผลิตภัณฑ์ที่แสดงผล
+      for (var i = countPage; i < countPage + perPage && i < data.length; i++) {
+        products.add(data[i]); // เพิ่มผลิตภัณฑ์ที่โหลดเข้ามาในรายการผลิตภัณฑ์
+      }
+
+      // เพิ่มค่าตัวนับของหน้าที่โหลดแล้ว
+      countPage += perPage;
+
+      print('countPage ${countPage}');
+    } catch (e) {
+      // จัดการข้อผิดพลาดและแสดงข้อความหรือบันทึกลงใน console
+      print('Error loading products: $e');
+    } finally {
+      // เมื่อโหลดข้อมูลเสร็จสิ้น ไม่ว่าจะสำเร็จหรือมีข้อผิดพลาด ตั้ง isLoading เป็น false
+      isLoading(false);
+    }
   }
 
   void increment() => count.value++;
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-import 'package:getx_tutorial/app/controllers/app_controller.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,13 +13,43 @@ class HomeView extends GetView<HomeController> {
         title: const Text('HomeView'),
         centerTitle: true,
       ),
-      body: Obx(() => ListView.builder(
-          itemCount: controller.products.length,
-          itemBuilder: (_, index) {
-            return ListTile(
-              title: Text(controller.products[index].title ?? ''),
-            );
-          })),
+      body: Obx(() {
+        return Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                controller:
+                    controller.scrollController, // กำหนด ScrollController
+                itemCount: controller.isLoading.value
+                    ? controller.products.length +
+                        1 // ถ้ากำลังโหลด เพิ่ม 1 สำหรับ Loading
+                    : controller
+                        .products.length, // ถ้าไม่โหลด แสดงแค่รายการผลิตภัณฑ์
+                itemBuilder: (_, index) {
+                  // ตรวจสอบว่าถึงรายการสุดท้ายของผลิตภัณฑ์หรือยัง
+                  if (index == controller.products.length) {
+                    // ถ้ากำลังโหลดข้อมูลเพิ่มเติมให้แสดง Loading indicator
+                    if (controller.isLoading.value) {
+                      return const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Center(child: CircularProgressIndicator()),
+                      );
+                    } else {
+                      // ถ้าไม่กำลังโหลด ไม่ต้องแสดงอะไรที่ตำแหน่งนี้
+                      return const SizedBox.shrink();
+                    }
+                  }
+
+                  // แสดงรายการผลิตภัณฑ์
+                  return ListTile(
+                    title: Text(controller.products[index].title ?? ''),
+                  );
+                },
+              ),
+            )
+          ],
+        );
+      }),
     );
   }
 }
